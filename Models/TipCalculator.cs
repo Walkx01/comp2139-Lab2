5a4dc70fa62084b5f7abe9308adcdf3d43d2abd0
Walkx01 <93958915+Walkx01@users.noreply.github.com>
2022-02-04 16:36:55 -0500

    Add project files.

---
+++ Models/TipCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lab2.Models
+{
+    public class TipCalculator
+    {
+        [Required (ErrorMessage ="Please enter a value for the cost of the meal")]
+        [Range(0.0, 1000.0, ErrorMessage ="cost of meal must be greater than 0 and less or equal to $1000")]
+       public double? MealCost { get; set; }
+
+        public double calculateTip( double percent)
+        {
+            if (MealCost.HasValue)
+            {
+                var tip = MealCost.Value * percent;
+                return tip;
+            }
+            else
+            {
+                return 0.0;
+            }
+        }
+
+      
+    }
+}
